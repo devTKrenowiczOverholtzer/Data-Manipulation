756469b5c768d1581ceb58f34071af5967173f3b
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-04 15:50:42 -0700

    MetaData

---
+++ DataInquiry.py
@@ -0,0 +1,9 @@
+#--------------------------------METADATA------------------------------------
+# TORI OVERHOLTZER
+# CS404: AI Data Analysis for Manufacturing , Agriculture and Energy
+# DESCRIPTION: Data Manipulation of wine quality based on physicochemical properties (Agricultural Application)
+# ENVIORMENT: Python 3, Pandas
+
+#Python line comments denoted by <#>
+
+
