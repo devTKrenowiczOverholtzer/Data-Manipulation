0d253ee5efd5f9092076c01431adb69ee05a8301
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-05 01:13:26 -0700

    Scatter

--- DataInquiry.py
+++ DataInquiry.py
@@ -232,6 +232,8 @@ import seaborn as sns
 ##plt.savefig("scatter_plot.png")
 ##plt.savefig("<foldername>/scatter_plot.png")
 
+
+
 #--------------------------------RESOURCES AND REFERENCES-------------------
 # DATASET: P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.Modeling wine preferences by data mining from physicochemical properties.In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236
 # CODE CONTRIBUTOR: Mary Everett, University of Idaho PHD
