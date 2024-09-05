81b02d78bbd4cf9afa918bbba5aa9c4d68e8ec4c
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-05 00:59:44 -0700

    SaveFigure

--- DataInquiry.py
+++ DataInquiry.py
@@ -229,6 +229,7 @@ plt.show()
 plt.clf()
 # matlab has all sorts of parameters can be set, change title, x,y axis
 
+plt.savefig("scatter_plot.png")
 
 #--------------------------------RESOURCES AND REFERENCES-------------------
 # DATASET: P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.Modeling wine preferences by data mining from physicochemical properties.In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236
