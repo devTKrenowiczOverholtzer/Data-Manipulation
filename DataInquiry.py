ded56e6c0c2e0d5538a5910205ffc7fd00f7d2ad
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-05 00:40:50 -0700

    Add Seaborn Visualization Library

--- DataInquiry.py
+++ DataInquiry.py
@@ -208,6 +208,8 @@ print(cleaned_df.loc[0,"quality"])
 # import data visualization libraries 
 import matplotlib.pyplot as plt
 # matlab has plotting library very common to use in python 
+import seaborn as sns
+
 
 #--------------------------------RESOURCES AND REFERENCES-------------------
 # DATASET: P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.Modeling wine preferences by data mining from physicochemical properties.In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236
